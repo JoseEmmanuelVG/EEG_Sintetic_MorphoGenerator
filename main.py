b0c2ad6e3f1e7e36bbb29a283e5a5474221d5b15
JoseEmmanuelVG <89156254+JoseEmmanuelVG@users.noreply.github.com>
2024-03-05 13:42:19 -0600

    Adding styles with Css in the assets folder

---
+++ main.py
@@ -0,0 +1,31 @@
+import dash
+from ui_definition import get_layout
+from callbacks_and_logic import register_callbacks
+from callbacks_fast_page import register_callbacks_fast
+from callbacks_detailed_page import register_callbacks_detailed  
+from waitress import serve
+
+from dash.dependencies import Output, Input
+from ui_definition import generacion_rapida_layout, generacion_detallada_layout, homepage_layout
+
+
+app = dash.Dash(__name__)
+app.layout = get_layout()
+
+@app.callback(Output('page-content', 'children'),
+              [Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == '/generacion-rapida':
+        return generacion_rapida_layout()
+    elif pathname == '/generacion-detallada':
+        return generacion_detallada_layout()
+    else:
+        return homepage_layout()
+
+
+register_callbacks_fast(app)
+register_callbacks_detailed(app)
+
+from waitress import serve
+serve(app.server, host="0.0.0.0", port=8080)
+
